77896e4b07cc98ee4cb754af72f7587461683191
spankitgaming2007 <spankitgaming2007@gmail.com>
2026-01-22 11:09:03 +0530

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-API_ID = 24041905          # yaha apna API ID likho
-API_HASH = "d7474bb9f159f88ffc7f734f2987e035"   # yaha apna API HASH
-BOT_TOKEN = "8346170767:AAF2ojws8jmAysnIdAbS-Mvpe10rkgNIgIc"  # yaha BotFather ka token
+API_ID = 24041905          
+API_HASH = "d7474bb9f159f88ffc7f734f2987e035"   
+BOT_TOKEN = "8346170767:AAF2ojws8jmAysnIdAbS-Mvpe10rkgNIgIc"  
+
